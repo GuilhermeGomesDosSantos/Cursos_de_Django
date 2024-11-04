f266e1c22755525d883ccbddc32701504ca5b318
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-11-03 22:22:30 -0300

    Incrementando o index

--- Curso de Django II - Alura/Alura Space/galeria/models.py
+++ Curso de Django II - Alura/Alura Space/galeria/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from datetime import datetime
+
 # Create your models here.
 class Fotografia(models.Model):
 
@@ -16,6 +18,7 @@ class Fotografia(models.Model):
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
     publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
